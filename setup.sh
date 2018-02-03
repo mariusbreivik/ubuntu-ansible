91e5b76a1afd5568799ddf6e8e9ac3afdcdcd8ef
Marius <marius.breivik@gmail.com>
2018-02-03 17:32:42 +0100

    fixed bug

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@
 echo 'Installing ansible'
 sudo apt-add-repository ppa:ansible/ansible -y
 sudo update
-sudo install software-properties-common ansible -y
+sudo apt install software-properties-common ansible -y
 
 sudo apt-cache policy ansible
 
