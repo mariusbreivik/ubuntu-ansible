9d8c4a94bd88be4bd6f276644368164117e280a6
Marius Breivik <marius.breivik@gmail.com>
2018-02-03 18:47:15 +0100

    minor changes

--- setup.sh
+++ setup.sh
@@ -5,9 +5,16 @@
 echo 'Installing ansible'
 sudo apt-add-repository ppa:ansible/ansible -y
 sudo apt update
-sudo apt install software-properties-common ansible -y
+sudo apt install software-properties-common ansible unzip -y
 
-sudo apt-cache policy ansible
+
+# need to clone the repo to get the rest of the files
+WORKDIR=/tmp
+wget https://github.com/mariusbreivik/ubuntu-ansible/archive/master.zip -P $WORKDIR
+cd $WORKDIR
+unzip master.zip devsetup/
+cd devsetup
+
 
 echo 'Ansible installed successfully'
 
