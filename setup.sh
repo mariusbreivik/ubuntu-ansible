7882712ab5615fb8bf3018138559014f991ba88b
Marius Breivik <marius.breivik@gmail.com>
2018-02-03 18:51:48 +0100

    added options

--- setup.sh
+++ setup.sh
@@ -12,11 +12,9 @@ sudo apt install software-properties-common ansible unzip -y
 WORKDIR=/tmp
 wget https://github.com/mariusbreivik/ubuntu-ansible/archive/master.zip -P $WORKDIR
 cd $WORKDIR
-unzip master.zip devsetup/
-cd devsetup
-
+unzip -o master.zip -d devsetup
 
 echo 'Ansible installed successfully'
 
 echo 'Starting installation scripts'
-ansible-playbook ubuntu.yaml
+ansible-playbook $WORKDIR/devsetup/ubuntu.yaml
